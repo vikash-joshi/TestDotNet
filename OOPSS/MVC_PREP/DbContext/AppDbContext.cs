f3b30b91fa9f99941f76395374771e62b1c6f8ef
vikashjoshi <vikashjoshi1651998@gmail.com>
2025-06-28 11:06:57 +0530

    new files updates

--- OOPSS/MVC_PREP/DbContext/AppDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using MVC_PREP.Models;
-
-public class AppDbContext : DbContext
-{
-    public DbSet<Student> Students { get; set; }
-}
--- OOPSS/MVC_PREP/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVC_PREP.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
